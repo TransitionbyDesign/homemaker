7088a8bbee54cbbdc28499d69517103b231f1034
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2020-09-11 15:28:48 +0100

    gatsby-config.js - configure gatsby-transformer-remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,14 @@ module.exports = {
       },
     },
     'gatsby-plugin-sass',
-
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/src/content`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
