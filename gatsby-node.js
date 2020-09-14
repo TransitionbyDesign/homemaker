72070ce1306495240956ea8a028ed7698265b545
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2020-09-14 19:20:22 +0100

    gatsby-node.js - generate markdown pages from src/content/posts/*.md

--- gatsby-node.js
+++ gatsby-node.js
@@ -23,3 +23,41 @@ exports.onCreateWebpackConfig = ({
     }
   })
 };
+
+// Build pages from markdown
+const path = require(`path`)
+exports.createPages = async ({ actions, graphql }) => {
+  const { createPage } = actions
+
+  const result = await graphql(`
+    query MyQuery {
+      allMarkdownRemark {
+        edges {
+          node {
+            id
+            parent {
+              ... on File {
+                relativePath
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+  if (result.errors) {
+    console.error(result.errors)
+  }
+
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    const filePath = path.parse(node.parent.relativePath);
+    const pagePath = path.format({dir: filePath.dir,
+                                  name: filePath.name,
+                                  ext: '.html'});
+    createPage({
+      path: pagePath,
+      component: path.resolve(`src/templates/post.js`),
+      context: { id: node.id },
+    });
+  })
+}
