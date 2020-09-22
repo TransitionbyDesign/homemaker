bf9e0014f5938ea8e917e8af708cd5314a9eccb7
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2020-09-22 23:20:00 +0100

    src/pages/* - reindent

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,13 +4,13 @@ import Layout from "../components/Layout"
 import notFoundStyles from '../styles/pages/404.module.scss'
 
 export default function NotFound() {
-    return (
-        <Layout page="404" bgColor="inherit">
-            <div className={notFoundStyles.notFound__container}>
-                <Link to="/">
-                    <h1>Sorry, couldn't find that page.</h1>
-                </Link>
-            </div>
-        </Layout>
-    )
+  return (
+    <Layout page="404" bgColor="inherit">
+      <div className={notFoundStyles.notFound__container}>
+        <Link to="/">
+          <h1>Sorry, couldn't find that page.</h1>
+        </Link>
+      </div>
+    </Layout>
+  )
 }
