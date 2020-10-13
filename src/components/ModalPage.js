1fcc7bebf0451c44af12e5cfdeb6ec52ae7c4510
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2020-10-13 22:29:04 +0100

    src/pages/modal.js - extract ModalPage function
    To abstract out the boilerplate.

---
+++ src/components/ModalPage.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import { ModalRoutingContext } from 'gatsby-plugin-modal-routing'
+import splash from '../styles/components/splash.module.scss';
+import cn from 'classnames';
+
+export default ({header, footer, children}) => (
+  <ModalRoutingContext.Consumer>
+    
+    {({ modal, closeTo }) => (
+      <div className={splash.wrapper}>
+        <header className={splash.header}>
+          {header}
+          
+          {modal ? (
+            <Link className={splash.closeLink} to={closeTo}>
+            &#x2A2F;
+            </Link>
+          ) : ''}
+        </header>
+        <div className={cn([splash.content])}>
+          {children}
+        </div>
+        <footer>
+          {footer}
+        </footer>
+      </div>
+    )}
+  </ModalRoutingContext.Consumer>
+);
