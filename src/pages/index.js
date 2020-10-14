0b2f5c05e61a8fab893e832113fdfbea12a9a8f5
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2020-10-14 23:24:07 +0100

    src/pages/map/index.js - move map index here, use info page as index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,39 @@
-import React from 'react';
-import { Link } from "gatsby";
-import MapLayout from "../components/MapLayout";
+import React from "react"
+import { Link } from 'gatsby'
+import Layout from "../components/Layout"
+import infoStyles from "../styles/pages/info.module.scss"
+import useSiteMetaData from "../static_queries/useSiteMetadata"
 
-const MapPage = () => {
+export default function Info() {
+  const { infoData } = useSiteMetaData()
   return (
-    <MapLayout>
-    </MapLayout>
-  );
+    <Layout className={infoStyles.layout}>
+      <section className={infoStyles.info_blurb}>
+        <h2>
+          <div dangerouslySetInnerHTML={{__html: infoData.description}}></div>
+          <div dangerouslySetInnerHTML={{__html: infoData.cta}}></div>
+        </h2>
+        <p><Link to="map" >Map</Link></p>
+        <ul>
+          <li>
+            <p>
+              <a href={`mailto:${infoData.contact.email}`}>Email: {infoData.contact.email}</a>
+            </p>
+          </li>
+          <li>
+            <p>
+              <a href={`https://twitter.com/${infoData.contact.twitter_handle}`}>
+                Twitter: @{infoData.contact.twitter_handle}
+              </a>
+            </p>
+          </li>
+          <li>
+            <p>
+              <a href={`https://github.com/${infoData.contact.github_handle}`}>Github: {infoData.contact.github_handle}</a>
+            </p>
+          </li>
+        </ul>
+      </section>
+    </Layout>
+  )
 }
-
-export default MapPage;
---
+++ src/pages/map/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from "gatsby";
+import MapLayout from "../../components/MapLayout";
+
+const MapPage = () => {
+  return (
+    <MapLayout>
+    </MapLayout>
+  );
+}
+
+export default MapPage;
