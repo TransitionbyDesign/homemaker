f298956c46dd061be6d530b3057dc3da0335003d
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2020-10-18 17:07:30 +0100

    src/components/ModalPage.js - deduplicate with Splash function

--- src/components/ModalPage.js
+++ src/components/ModalPage.js
@@ -6,50 +6,35 @@ import mapLayout from '../styles/components/mapLayout.module.scss';
 import splash from '../styles/components/splash.module.scss';
 import cn from 'classnames';
 
-export default ({header, footer, children}) => (
-  <ModalRoutingContext.Consumer>        
-    {
-      ({ modal, closeTo }) => {
-        if (modal) {
-          return (
-            <div className={splash.wrapper}>
-              <header className={splash.header}>
-                {header}
-                
-                <Link className={splash.closeLink} to={closeTo}>
-                  &#x2A2F;
-                </Link>
-              </header>
-              <div className={cn([splash.content])}>
-                {children}
-              </div>
-              <footer>
-                {footer}
-              </footer>
-            </div>
-          );
-        }
-        else {
-          return (  // As above, but we must insert the map behind
-            <MapLayout className={mapLayout.modal}>
-              <div className={splash.wrapper}>
-                <header className={splash.header}>
-                  {header}                  
-                  <Link className={splash.closeLink} to="/map/">
-                    &#x2A2F;
-                  </Link>
-                </header>
-                <div className={cn([splash.content])}>
-                  {children}
-                </div>
-                <footer>
-                  {footer}
-                </footer>
-              </div>
-            </MapLayout>
-          );
-        }
-      }
-    }
+const Splash = ({ header, footer, children, closeTo }) => (
+  <div className={splash.wrapper}>
+    <header className={splash.header}>
+      {header}
+      
+      <Link className={splash.closeLink} to={closeTo}>
+        &#x2A2F;
+      </Link>
+    </header>
+    <div className={cn([splash.content])}>
+      {children}
+    </div>
+    <footer>
+      {footer}
+    </footer>
+  </div>
+);
+
+
+export default ({ header, footer, children }) => (
+  <ModalRoutingContext.Consumer>
+  {
+    ({ modal, closeTo }) =>
+      modal?
+      <Splash header={header} footer={footer} closeTo={closeTo} >{children}</Splash>
+      : // As above, but we must insert the map behind, and close to /map/
+      <MapLayout className={mapLayout.modal}>
+        <Splash header={header} footer={footer} closeTo="/map/" >{children}</Splash>
+      </MapLayout>
+  }
   </ModalRoutingContext.Consumer>
 );
