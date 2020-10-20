b82cf047a9fcd57e373d839d5b789a95bb5657d4
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2020-10-20 17:51:35 +0100

    src/components/ModalPage.js - extract Splash function to own file

--- src/components/ModalPage.js
+++ src/components/ModalPage.js
@@ -1,31 +1,8 @@
 import React from 'react'
-import { Link } from 'gatsby'
 import { ModalRoutingContext } from 'gatsby-plugin-modal-routing'
 import MapLayout from '../components/MapLayout'
 import mapLayout from '../styles/components/mapLayout.module.scss';
-import splash from '../styles/components/splash.module.scss';
-import cn from 'classnames';
-
-const Splash = ({ header, footer, children, closeTo }) => (
-  <div className={splash.outerWrapper}>
-    <div className={splash.innerWrapper}>
-      <header className={splash.header}>
-        {header}
-        
-        <Link className={splash.closeLink} to={closeTo}>
-          &#x2A2F;
-        </Link>
-      </header>
-      <div className={cn([splash.content])}>
-        {children}
-      </div>
-      <footer>
-        {footer}
-      </footer>
-    </div>
-  </div>
-);
-
+import Splash from '../components/Splash'
 
 export default ({ header, footer, children }) => (
   <ModalRoutingContext.Consumer>
