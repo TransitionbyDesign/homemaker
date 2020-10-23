9877010e754d9edde5571d8da236ef28ce6f60c7
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2020-10-23 20:24:54 +0100

    src/pages/index.js - reconstruct to match Jas' design

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,67 @@
 import React from "react"
 import { Link } from 'gatsby'
+import { useLocation } from '@reach/router'
 import Layout from "../components/Layout"
-import infoStyles from "../styles/pages/info.module.scss"
+import Splash from "../components/Splash"
+import SocialLink from "../components/SocialLink"
+import splashStyles from "../styles/components/splash.module.scss"
+import mapLayoutStyles from "../styles/components/mapLayout.module.scss"
+import landingStyles from "../styles/pages/landing.module.scss"
 import useSiteMetaData from "../static_queries/useSiteMetadata"
+import cn from "classnames"
+import twitterIcon from "../icons/social_icon_twitter.svg"
+import linkedInIcon from "../icons/social_icon_linkedin.svg"
+import facebookIcon from "../icons/social_icon_facebook.svg"
 
-export default function Info() {
+
+export default () => {
   const { infoData } = useSiteMetaData()
+  
+  // Ensure that whatever happens, the modal state flag is set
+  // This ensures the pages and layouts are correct.
+  const location = useLocation();
+  location.state = { ...location.state = {}, modal: true };
   return (
-    <Layout className={infoStyles.layout}>
-      <section className={infoStyles.info_blurb}>
-        <h2>
-          <div dangerouslySetInnerHTML={{__html: infoData.description}}></div>
-          <div dangerouslySetInnerHTML={{__html: infoData.cta}}></div>
-        </h2>
-        <p><Link to="map" >Map</Link></p>
-        <ul>
-          <li>
-            <p>
-              <a href={`mailto:${infoData.contact.email}`}>Email: {infoData.contact.email}</a>
-            </p>
-          </li>
-          <li>
-            <p>
-              <a href={`https://twitter.com/${infoData.contact.twitter_handle}`}>
-                Twitter: @{infoData.contact.twitter_handle}
-              </a>
-            </p>
-          </li>
-          <li>
-            <p>
-              <a href={`https://github.com/${infoData.contact.github_handle}`}>Github: {infoData.contact.github_handle}</a>
-            </p>
-          </li>
-        </ul>
-      </section>
+    <Layout className={mapLayoutStyles.map_layout}>
+      <div className={mapLayoutStyles.overlay}>
+        <Splash
+          header="Welcome"
+          footer={
+            <>
+              <Link to="/map" className={splashStyles.button}>EXPLORE THE MAP</Link>
+              {/*<SocialLink to={`mailto:${infoData.contact.email}`}
+                  logo={}
+                  >
+                  Email: {infoData.contact.email}
+                  </SocialLink>*/}
+              <div className={splashStyles.linkIcons}>
+                <SocialLink to={`https://twitter.com/${infoData.contact.twitter_handle}`}
+                  logo={twitterIcon} alt="Twitter"
+                >
+                  Twitter: @{infoData.contact.twitter_handle}
+                </SocialLink>
+                <SocialLink to={`https://facebook.com/infoData.contact.facebook_page`}
+                  logo={facebookIcon} alt="Facebook"
+                >
+                  Facebook: @{infoData.contact.twitter_handle}
+                </SocialLink>
+                <SocialLink to={`https://github.com/${infoData.contact.github_handle}`}
+                  logo={linkedInIcon} alt="Linked In"
+                >
+                  Linked In: {infoData.contact.github_handle}
+                </SocialLink>
+              </div>
+            </>
+          }
+        >
+          <h1>Welcome to Homemaker Oxford</h1>
+          
+          <div
+            className={splashStyles.columned}
+            dangerouslySetInnerHTML={{__html: infoData.description}} />
+          
+        </Splash>
+      </div>
     </Layout>
   )
 }
