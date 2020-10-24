6a97d9e883227eb0c05e71bcb9df84e4ac9d05ba
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2020-10-24 18:09:09 +0100

    components/MapLayout.js - support all the icons
    So active/inactive problem/solution, text/audio/video

--- src/components/MapLayout.js
+++ src/components/MapLayout.js
@@ -5,8 +5,12 @@ import mapLayout from "../styles/components/mapLayout.module.scss";
 import Map from '../components/Map';
 import { Marker, Popup } from "react-leaflet";
 import data from "../data.json";
-import textIcon from "../icons/text_ptr_blue.svg";
-import audioIcon from "../icons/audio_ptr_blue.svg";
+import textIconBlue from "../icons/text_ptr_blue.svg";
+import textIconPink from "../icons/text_ptr_pink.svg";
+import audioIconBlue from "../icons/audio_ptr_blue.svg";
+import audioIconPink from "../icons/audio_ptr_pink.svg";
+import videoIconBlue from "../icons/video_ptr_blue.svg";
+import videoIconPink from "../icons/video_ptr_pink.svg";
 import cn from 'classnames';
 import L from 'leaflet';
 
@@ -17,28 +21,92 @@ const mapSettings = {
   zoom: 12,
   minZoom: 12,
 };
+const pinSize = 32;
+const activePinSize = 64;
 
 let icons = null;
 
-function icon(pin, activePin) {
-  if (pin === activePin) {
-    return icons.Audio;
-  }
-  else {
-    return icons.Text;
-  }
+function selectIcon(pin, activePin) {
+  if (!icons) return;
+
+  const mediaIcons = icons[pin.media];
+  if (!mediaIcons) return;
+
+  const typeIcons = mediaIcons[pin.type];
+  if (!typeIcons) return;
+  
+  return pin === activePin?
+         typeIcons.active : typeIcons.inactive;
 }
 
 function buildIcons() {
   return {
-    Text: new L.Icon({
-      iconUrl: textIcon,
-      iconSize: [32, 32]
-    }),
-    Audio: new L.Icon({
-      iconUrl: audioIcon,
-      iconSize: [64, 64]
-    }),
+    text: {
+      problem: {
+        inactive: new L.Icon({
+          iconUrl: textIconBlue,
+          iconSize: [pinSize, pinSize]
+        }),
+        active: new L.Icon({
+          iconUrl: textIconBlue,
+          iconSize: [activePinSize, activePinSize]
+        }),
+      },
+      solution: {
+        inactive: new L.Icon({
+          iconUrl: textIconPink,
+          iconSize: [pinSize, pinSize]
+        }),
+        active: new L.Icon({
+          iconUrl: textIconPink,
+          iconSize: [activePinSize, activePinSize]
+        }),
+      },
+    },
+    audio: {
+      problem: {
+        inactive: new L.Icon({
+          iconUrl: audioIconBlue,
+          iconSize: [pinSize, pinSize]
+        }),
+        active: new L.Icon({
+          iconUrl: audioIconBlue,
+          iconSize: [activePinSize, activePinSize]
+        }),
+      },
+      solution: {
+        inactive: new L.Icon({
+          iconUrl: audioIconPink,
+          iconSize: [pinSize, pinSize]
+        }),
+        active: new L.Icon({
+          iconUrl: audioIconPink,
+          iconSize: [activePinSize, activePinSize]
+        }),
+      },
+    },    
+    video: {
+      problem: {
+        inactive: new L.Icon({
+          iconUrl: videoIconBlue,
+          iconSize: [pinSize, pinSize]
+        }),
+        active: new L.Icon({
+          iconUrl: videoIconBlue,
+          iconSize: [activePinSize, activePinSize]
+        }),
+      },
+      solution: {
+        inactive: new L.Icon({
+          iconUrl: videoIconPink,
+          iconSize: [pinSize, pinSize]
+        }),
+        active: new L.Icon({
+          iconUrl: videoIconPink,
+          iconSize: [activePinSize, activePinSize]
+        }),
+      },
+    },    
   };
 }
 
@@ -59,10 +127,11 @@ export default (props) => {
         <div className={cn(mapLayout.map, {[mapLayout.disabled]: props.mapDisabled})}>
           <Map settings={mapSettings}>
             {data.features.map(pin => {
-              if (icons && icons[pin.type]) {
+              const icon = selectIcon(pin, activePin)
+              if (icon) {
                 return (<Marker
                           key={pin.properties.PIN_ID}
-                          icon={icon(pin, activePin)}
+                          icon={icon}
                           position={[
                             pin.geometry.coordinates[0],
                             pin.geometry.coordinates[1]
