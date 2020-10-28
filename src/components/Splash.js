8c11342fc53b5b6dba63c515c66b9ecfba8d58c8
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2020-10-28 22:27:11 +0000

    components/Splash.js - use svg cross icon

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -11,7 +11,10 @@ const Splash = ({ header, footer, children, closeTo, className }) => (
 
         { closeTo &&
           <Link className={splash.closeLink} to={closeTo}>
-            &#x2A2F;
+            <svg viewBox="0 0 386.667 386.667"
+              xmlns="http://www.w3.org/2000/svg">
+              <path d="m386.667 45.564-45.564-45.564-147.77 147.769-147.769-147.769-45.564 45.564 147.769 147.769-147.769 147.77 45.564 45.564 147.769-147.769 147.769 147.769 45.564-45.564-147.768-147.77z"/>
+            </svg>
           </Link>
         }
       </header>
