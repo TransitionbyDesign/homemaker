e74bb5920aaae1c7697eb2ece32d49ba95268f3d
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2020-10-29 22:27:58 +0000

    components/MapLayout.js - include region posts as geojson

--- src/static_queries/useMapData.js
+++ src/static_queries/useMapData.js
@@ -13,6 +13,7 @@ export default function useMapData() {
               title
               latitude
               longitude
+              geojson
               apposition
               is_published
               hero_image {
