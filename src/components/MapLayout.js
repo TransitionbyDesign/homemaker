18aeeaa62d60e342c38ac63d5f47c6b6ffd0f659
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2020-10-30 18:05:52 +0000

    components/MapLayout.js - use Stamen TonerLite tiles
    https://github.com/TransitionbyDesign/homemaker/issues/31

--- src/components/MapLayout.js
+++ src/components/MapLayout.js
@@ -3,7 +3,7 @@ import { Link, navigate } from 'gatsby'
 import Layout from "../components/Layout";
 import mapLayout from "../styles/components/mapLayout.module.scss";
 import Map from '../components/Map';
-import { Marker, Popup, Tooltip, GeoJSON } from "react-leaflet";
+import { TileLayer, Marker, Popup, Tooltip, GeoJSON } from "react-leaflet";
 import articleIconBlue from "../icons/text_ptr_blue.svg";
 import articleIconPink from "../icons/text_ptr_pink.svg";
 import audioIconBlue from "../icons/audio_ptr_blue.svg";
@@ -107,6 +107,14 @@ export default (props) => {
         
         <div className={cn(mapLayout.map, {[mapLayout.disabled]: props.mapDisabled})}>
           <Map settings={mapSettings}>
+            <TileLayer
+              url='https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}'
+              attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	            subdomains='abcd'
+	            minZoom='0'
+	            maxZoom='20'
+	            ext='png'
+            />
             {mapData
               .filter((item) => item?.node?.frontmatter?.is_published)
               .map(item => {
