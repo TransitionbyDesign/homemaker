7deb3b4167e1083b703774a4f4738ab4b1f9e497
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2020-10-31 18:44:44 +0000

    components/Splash.js - split SVG icon out to CloseIcon.js

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 import splash from '../styles/components/splash.module.scss';
 import Window from './Window.js'
+import CloseIcon from './CloseIcon'
 import cn from 'classnames';
 
 const Splash = ({ header, footer, children, closeTo, className }) => (
@@ -14,10 +15,7 @@ const Splash = ({ header, footer, children, closeTo, className }) => (
           {header}
           {!closeTo ? '' :
            <Link className={splash.closeLink} to={closeTo}>
-             <svg style={{width: "1em"}} viewBox="0 0 386.667 386.667"
-               xmlns="http://www.w3.org/2000/svg">
-               <path d="m386.667 45.564-45.564-45.564-147.77 147.769-147.769-147.769-45.564 45.564 147.769 147.769-147.769 147.77 45.564 45.564 147.769-147.769 147.769 147.769 45.564-45.564-147.768-147.77z"/>
-             </svg>
+             <CloseIcon/>
            </Link>
           }
         </>
