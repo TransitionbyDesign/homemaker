964036f72f84ab07437e13d78fadd877eee6f5ae
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2020-11-05 23:19:34 +0000

    components/CloseIcon.js - ajust SVG so that JSX is happy
    JSX doesn't like the class attribute, wants it to be className.

--- src/components/CloseIcon.js
+++ src/components/CloseIcon.js
@@ -7,6 +7,6 @@ export default () => (
     strokeLinejoin: "round", strokeWidth: "2px",  width: "1em",
   }}
     viewBox="0 0 16.4 16.4">
-    <g id="Layer_2" data-name="Layer 2"><g id="comments_feedback" data-name="comments feedback"><line class="cls-1" x1="15.4" y1="1" x2="1" y2="15.4"/><line class="cls-1" x1="1" y1="1" x2="15.4" y2="15.4"/></g></g>
+    <g id="Layer_2" data-name="Layer 2"><g id="comments_feedback" data-name="comments feedback"><line className="cls-1" x1="15.4" y1="1" x2="1" y2="15.4"/><line className="cls-1" x1="1" y1="1" x2="15.4" y2="15.4"/></g></g>
   </svg>
 )
