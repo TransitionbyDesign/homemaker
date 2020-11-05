af51287d1b2736fe46c52eba77ba060c7d57fc8f
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2020-11-05 15:25:49 +0000

    components/SocialLink.js - mark social media link as 'noreferrer' links
    warning  Using target="_blank" without rel="noreferrer" is a security risk: see
    https://html.spec.whatwg.org/multipage/links.html#link-type-noopener  react/jsx-no-target-blank

--- src/components/SocialLink.js
+++ src/components/SocialLink.js
@@ -4,7 +4,7 @@ import socialLinkStyles from '../styles/components/socialLink.module.scss';
 const SocialLink = ({ logo, alt, title, to, children }) => (
   <a
     className={socialLinkStyles.link}
-    href={to} target="_blank">
+    href={to} target="_blank" ref="noreferrer">
     <span className={socialLinkStyles.altText}>{children}</span>
     <img title={title} className={socialLinkStyles.img} src={logo} alt={alt} />
   </a>
