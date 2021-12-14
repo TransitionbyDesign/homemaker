dc93048a23e5cdf01f3811719e9bd821c8216ceb
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2021-12-14 10:53:20 +0000

    gatsby-node.js - try adding @dontInfer directive to explicit typespec
    As suggested in build error, referencing:
    
    https://www.gatsbyjs.com/docs/reference/config-files/actions/#createTypes

--- gatsby-node.js
+++ gatsby-node.js
@@ -38,37 +38,37 @@ exports.createSchemaCustomization = ({ actions }) => {
   })
 
 
-  // Add a type definition for this field which marks
-  // `summary` as a markdown field. Note, `MarkdownRemarkFrontmatter`
-  // is empirically the type name of frontmatter objects (and not
-  // `Frontmatter` as in some of the docs). We define some other fields
-  // which can be optional so as to avoid errors if Gatsby does not
-  // find any examples and therefore can't infer their existence.
-  // Note well: any File fields are an exception, as I've not
-  // discovered how to define these correctly. (Simply specifying, e.g.
-  // `hero_image: File` does not work.) I suspect a bug in Gatsby.
-  // I find myself caught between errors related to File fields being objects,
-  // which need at least one subfield in the query, and errors related to
-  // the fields being interpreted as strings, which cannot have subfields,
-  // or simply being missing. I also get "Cannot read property 'contentDigest'
-  // of undefined` for publicURL", as in this thread:
-  //
-  // https://spectrum.chat/gatsby-js/general/cannot-read-property-contentdigest-of-undefined-for-publicurl~b3f62193-936b-4b84-acea-66a6f4f4876f
-  //
-  // Overall, *not* declaring image/file fields expicitly and allowing Gatsby
-  // to infer them seems to work least badly, at least for now. I do worry that
-  // this is fragile, however, I don't seem to have a lot of choice, as I don't have
-  // the time to write a full bug report for the Gatsby project and/or experiment further.
-  //
-  // [later] Indeed it is fragile.
-  // I believe that the order in which gatsby encounters the articles
-  // changes the outcome (success, or one of various errors) by changing
-  // the way it infers the fields - sometimes inferring a string and sometimes
-  // inferring a File. This may be a consequence of custom_icon and
-  // hero_image being optional, I think, and therefore not consistently a file.
-  //  
-  actions.createTypes(`
-    type MarkdownRemarkFrontmatter {
+    // Add a type definition for this field which marks
+    // `summary` as a markdown field. Note, `MarkdownRemarkFrontmatter`
+    // is empirically the type name of frontmatter objects (and not
+    // `Frontmatter` as in some of the docs). We define some other fields
+    // which can be optional so as to avoid errors if Gatsby does not
+    // find any examples and therefore can't infer their existence.
+    // Note well: any File fields are an exception, as I've not
+    // discovered how to define these correctly. (Simply specifying, e.g.
+    // `hero_image: File` does not work.) I suspect a bug in Gatsby.
+    // I find myself caught between errors related to File fields being objects,
+    // which need at least one subfield in the query, and errors related to
+    // the fields being interpreted as strings, which cannot have subfields,
+    // or simply being missing. I also get "Cannot read property 'contentDigest'
+    // of undefined` for publicURL", as in this thread:
+    //
+    // https://spectrum.chat/gatsby-js/general/cannot-read-property-contentdigest-of-undefined-for-publicurl~b3f62193-936b-4b84-acea-66a6f4f4876f
+    //
+    // Overall, *not* declaring image/file fields expicitly and allowing Gatsby
+    // to infer them seems to work least badly, at least for now. I do worry that
+    // this is fragile, however, I don't seem to have a lot of choice, as I don't have
+    // the time to write a full bug report for the Gatsby project and/or experiment further.
+    //
+    // [later] Indeed it is fragile.
+    // I believe that the order in which gatsby encounters the articles
+    // changes the outcome (success, or one of various errors) by changing
+    // the way it infers the fields - sometimes inferring a string and sometimes
+    // inferring a File. This may be a consequence of custom_icon and
+    // hero_image being optional, I think, and therefore not consistently a file.
+    //  
+    actions.createTypes(`
+    type MarkdownRemarkFrontmatter @dontInfer {
       title: String!
       summary: String @md
       apposition: String
@@ -81,7 +81,7 @@ exports.createSchemaCustomization = ({ actions }) => {
       custom_icon: File
 #      hero_image: File # inferrence works better for now
     }
-  `)
+    `)
 }
 
 module.exports.onCreateNode = ({ node, actions }) => {
